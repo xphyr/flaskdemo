a17f3c17525b4fa44966291a517ccb3fae5b4308
Mark DeNeve <xphyr@users.noreply.github.com>
2020-09-28 18:22:51 -0400

    testing a change through the pipeline

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 
 app = Flask(__name__)
 title = "TODO sample application with Flask and MongoDB"
-heading = "TODO Reminder with Flask and MongoDB"
+heading = "TODO Reminder with Flask and MongoDB - by mark"
 
 mongoserver = os.getenv("MONGODB_SERVICE_HOST", default = "127.0.0.1")
 mongoport = os.getenv("MONGODB_SERVICE_PORT", default = "27017")
